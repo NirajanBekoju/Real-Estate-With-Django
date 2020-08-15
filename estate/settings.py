dacda989cd63aab68a2b28b48e86daa07e288741
Nirajan Bekoju <nirajan.bekoju@gmail.com>
2020-08-15 18:16:30 +0545

    Contact and Inquiries Completed

--- estate/settings.py
+++ estate/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'contacts',
     'listings',
     'realtors',
     'pages',
@@ -128,3 +129,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# Email Config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'nirajan.bekoju@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
